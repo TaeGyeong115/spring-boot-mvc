0bb6afd65eea96dfbe85b866ca32eb7b1660d878
Taylor <115taegyeong@gmail.com>
2024-06-26 20:57:18 +0900

    feat: static resource

--- src/main/java/io/taylor/springbootmvc/SpringBootMVCApplication.java
+++ src/main/java/io/taylor/springbootmvc/SpringBootMVCApplication.java
@@ -1,10 +1,11 @@
 package io.taylor.springbootmvc;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootMVCApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(SpringBootMVCApplication.class, args);
     }
 }
--- src/main/java/io/taylor/springbootmvc/config/WebConfig.java
+++ src/main/java/io/taylor/springbootmvc/config/WebConfig.java
@@ -2,10 +2,17 @@ package io.taylor.springbootmvc.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 // @EnableWebMvc // Web MVC 모든 설정을 제거함
 public class WebConfig implements WebMvcConfigurer {
-
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/m/**")
+                .addResourceLocations("classpath:/m/")
+                .setCachePeriod(20);
+    }
 }
