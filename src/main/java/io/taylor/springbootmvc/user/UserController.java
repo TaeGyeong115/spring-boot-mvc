b301a88416a819f0f5adbefb47c97a5ce6c2979a
Taylor <115taegyeong@gmail.com>
2024-06-27 01:03:19 +0900

    feat: thymeleaf

--- src/main/java/io/taylor/springbootmvc/user/UserController.java
+++ src/main/java/io/taylor/springbootmvc/user/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
-    @GetMapping("/hello")
+    @GetMapping("/user/hello")
     public String hello() {
         return "hello";
     }
